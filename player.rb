5db5610fae0d449135cb75fb90f8389c8bb42b66
Manjit Singh <manjitsingh23@gmail.com>
2017-03-03 01:58:57 +0000

    Constants created

--- player.rb
+++ player.rb
@@ -1,8 +1,12 @@
 class Player
 
+  ROTATION_SPEED = 3
+  ACCELERATION = 2
+  FRICTION = 0.9
+
   def initialize(window)
     @x = 200
-    @y = 200
+    @y = 200 
     @angle = 0
     @image = Gosu::Image.new('images/ship.png')
     @velocity_x = 0
@@ -14,22 +18,22 @@ class Player
   end
 
   def turn_right
-    @angle += 3
+    @angle += ROTATION_SPEED
   end
 
  def turn_left
-   @angle -= 3
+   @angle -= ROTATION_SPEED
  end 
 
  def accelerate
-   @velocity_x += Gosu.offset_x(@angle, 2)
-   @velocity_y += Gosu.offset_y(@angle, 2)
+   @velocity_x += Gosu.offset_x(@angle, ACCELERATION)
+   @velocity_y += Gosu.offset_y(@angle, ACCELERATION)
  end
 
  def move
    @x += @velocity_x
    @y += @velocity_y
-   @velocity_x *=0.9
-   @velocity_y *=0.9
+   @velocity_x *= FRICTION
+   @velocity_y *= FRICTION
  end
 end
--- sector_five.rb
+++ sector_five.rb
@@ -3,22 +3,25 @@ require_relative 'player'
 
 class SectorFive < Gosu::Window
 
-def initialize
-  super(800, 600)
-  self.caption = "Sector Five"
-  @player = Player.new(self)
-end
+  WIDTH = 800
+  HEIGHT = 600
 
-def draw
-  @player.draw
-end
+  def initialize
+    super(WIDTH, HEIGHT)
+    self.caption = "Sector Five"
+    @player = Player.new(self)
+  end
 
-def update
-  @player.turn_left if button_down?(Gosu::KbLeft)
-  @player.turn_right if button_down?(Gosu::KbRight)
-  @player.accelerate if button_down?(Gosu::KbUp)
-  @player.move
-end
+  def draw
+    @player.draw
+  end
+
+  def update
+    @player.turn_left if button_down?(Gosu::KbLeft)
+    @player.turn_right if button_down?(Gosu::KbRight)
+    @player.accelerate if button_down?(Gosu::KbUp)
+    @player.move
+  end
 
 end
 
