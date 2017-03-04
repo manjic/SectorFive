dcf02813cd016f2d03f05bfb35a5858c8d16e7f4
Manjit Singh <manjitsingh23@gmail.com>
2017-03-04 00:52:22 +0000

    Added single enemy ship with basic movement form top to bottom of window

--- sector_five.rb
+++ sector_five.rb
@@ -1,5 +1,6 @@
 require 'gosu'
 require_relative 'player'
+require_relative 'enemy'
 
 class SectorFive < Gosu::Window
 
@@ -10,10 +11,12 @@ class SectorFive < Gosu::Window
     super(WIDTH, HEIGHT)
     self.caption = "Sector Five"
     @player = Player.new(self)
+    @enemy = Enemy.new(self)
   end
 
   def draw
     @player.draw
+    @enemy.draw
   end
 
   def update
@@ -21,6 +24,7 @@ class SectorFive < Gosu::Window
     @player.turn_right if button_down?(Gosu::KbRight)
     @player.accelerate if button_down?(Gosu::KbUp)
     @player.move
+    @enemy.move
   end
 
 end
