406ce1822df9c1ed9b6ba855996023ee20074bb6
Manjit Singh <manjitsingh23@gmail.com>
2017-04-13 23:31:38 +0100

    Deleting of unecessary data from arrays

--- sector_five.rb
+++ sector_five.rb
@@ -55,7 +55,18 @@ class SectorFive < Gosu::Window
             @explosions.push Explosion.new(self, enemy.x, enemy.y)
           end
         end
-    end
+      end
+      @explosions.dup.each do |explosion|
+        @explosions.delete explosion if explosion.finished
+      end
+      @enemies.dup.each do |enemy| 
+        if enemy.y > HEIGHT + enemy.radius
+          @enemies.delete enemy
+        end
+      end
+      @bullets.dup.each do |bullet|
+        @bullets.delete bullet unless bullet.onscreen?
+      end
   end
 
   def button_down(id)
